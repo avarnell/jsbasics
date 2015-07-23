1534bcc979e70b36bdd3953beb8671bcc44fe10e
Andrew <avarnell@ago.org>
2015-07-23 15:17:02 -0600

    added Madlib

--- js/main.js
+++ js/main.js
@@ -2,6 +2,30 @@
 // it will ask them for their name, and then respond with "Hello [NAME_GOES_HERE]!"
 
 $(document).ready(function(){
-var name = prompt("Who goes there?");
-  alert ("Oh, why hello, " + name+"!");
-})
+  var name = prompt("Who goes there?");
+  alert ("Oh, why hello, " + name+"! We are going to try and do some stuff like push your name to elements on the page.");
+  $('.name').text(name);
+
+  var button = document.getElementById('ready');
+  function onClick(event) {
+    var verbing = document.getElementById("verbing").value;
+    var place = document.getElementById("place").value;
+    var adj = document.getElementById("adj").value;
+  
+    $('.verbing').text(verbing);
+    $('.place').text(place);
+    $('.adj').text(adj);
+  }
+  button.addEventListener('click', onClick);
+
+
+
+
+
+// var verbing
+
+// var place
+
+// var adj
+
+});
